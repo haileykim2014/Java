91bce1d73755744a5e6a78214d5027d250d12b49
yh <yh@DESKTOP-AD3VHGH>
2020-11-30 18:07:33 +0900

    배열로 구구단 구현

---
+++ src/Gugudan1130.java
@@ -0,0 +1,23 @@
+
+public class Gugudan1130 {
+
+	public static void main(String[] args) {
+    int[] result= new int[9];
+    for(int i=0;i<result.length; i++) {
+    	result[i]=2*(i+1);
+    }
+    for(int i=0;i<result.length;i++) {
+    	System.out.println(result[i]);
+    }
+    
+    int[] times3= new int[9];
+    for(int i=0;i<times3.length; i++) {
+    	times3[i]=3*(i+1);
+    }
+    for(int i=0;i<times3.length;i++) {
+    	System.out.println(times3[i]);
+    }
+
+ 
+}
+}
