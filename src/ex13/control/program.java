0a9784cf55ed9ce2ac7937a4d0c6cd0b9aa495aa
yh <yh@DESKTOP-AD3VHGH>
2020-12-01 17:08:40 +0900

    for 문 반복문

---
+++ src/ex13/control/program.java
@@ -0,0 +1,17 @@
+package ex13.control;
+
+import java.util.Scanner;
+
+
+public class program {
+
+
+	public static void main(String args[]) { // i=1,2,4,8,16
+		for(int i=10; i>=1; i--) {//괄호 {}안의 문장을 5번반복
+			System.out.println(i);
+		
+		}
+		
+	}
+	
+}
