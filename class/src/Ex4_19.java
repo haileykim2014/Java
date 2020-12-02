36e3af726a873c770d189eb5a39ac62d7102f168
yh <yh@DESKTOP-AD3VHGH>
2020-12-02 17:33:15 +0900

    반복문의 중첩 변수,반복문,조건문,연산

--- class/src/Ex4_19.java
+++ class/src/Ex4_19.java
@@ -3,10 +3,11 @@ public class Ex4_19 {
 
 	public static void main(String[] args) {
 
-		for(int i=0;i<10;i++) {
-			if(i==5)
-				continue;
-			System.out.println("I can do it"+i);
+
+for(int i=0;i<10;i++) {
+	for(int j=0;j<10;j++) {
+		System.out.println(i+""+j);
+			}
       }
 	
 	}
