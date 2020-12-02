7e73cc2d8e0817132b63316cf687b43dde107d40
yh <yh@DESKTOP-AD3VHGH>
2020-12-02 17:04:14 +0900

    break 문 (if문안에 break) i가 4일경우 if(5==5)이므로 참이다- break으로 가고 반복멈춤

---
+++ class/src/Ex4_19.java
@@ -0,0 +1,12 @@
+
+public class Ex4_19 {
+
+	public static void main(String[] args) {
+      for(int i=0; i<10;i++) {
+    	  if(i==5)
+    		  break;
+    	  System.out.println("coding everybody"+i);
+      }
+	
+	}
+}
