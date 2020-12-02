1f839338afa9526c34733617c94da20db140d382
yh <yh@DESKTOP-AD3VHGH>
2020-12-02 17:12:10 +0900

    반복분 continue
    I can do it0
    I can do it1
    I can do it2
    I can do it3
    I can do it4
    I can do it6
    I can do it7
    I can do it8
    I can do it9

--- class/src/Ex4_19.java
+++ class/src/Ex4_19.java
@@ -2,10 +2,11 @@
 public class Ex4_19 {
 
 	public static void main(String[] args) {
-      for(int i=0; i<10;i++) {
-    	  if(i==5)
-    		  break;
-    	  System.out.println("coding everybody"+i);
+
+		for(int i=0;i<10;i++) {
+			if(i==5)
+				continue;
+			System.out.println("I can do it"+i);
       }
 	
 	}
