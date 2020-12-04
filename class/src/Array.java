358e85ca20ea8b28a9661b644b3f34312bbca89f
yh <yh@DESKTOP-AD3VHGH>
2020-12-04 15:23:32 +0900

    배열연습

---
+++ class/src/Array.java
@@ -0,0 +1,12 @@
+
+public class Array {
+	
+	public static void main(String[] arg) {
+		int[] scores = {83,90,87};
+		
+		System.out.println("scores[0]:" + scores[0]);
+		System.out.println("scores[1]:" + scores[1]);
+		System.out.println("scores[2]:" + scores[2]);
+
+	}
+}
