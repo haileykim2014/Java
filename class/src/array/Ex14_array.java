3c4edee6170f579969c16a7349861e698405b32f
yh <yh@DESKTOP-AD3VHGH>
2020-12-06 19:24:27 +0900

    조건식에 자동으로 인덱스길이반영

--- class/src/array/Ex14_array.java
+++ class/src/array/Ex14_array.java
@@ -4,10 +4,10 @@ package array;
 public class Ex14_array {
 
 	public static void main(String[] args) {
-		int[] arr=new int[10];
+		int[] arr=new int[6];//길이자동으로 
 		System.out.println("arr.length="+arr.length);
 		
-		for(int i=0;i<10;i++) {
+		for(int i=0;i<arr.length;i++) {
 			System.out.println("arr["+i+"]="+arr[i]);
 		}
 		
