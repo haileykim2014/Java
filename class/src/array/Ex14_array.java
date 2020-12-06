7cc5f7b01eb0417a79aab3da4a3d53d33d5efd0d
yh <yh@DESKTOP-AD3VHGH>
2020-12-06 19:23:15 +0900

    배열 인덱스범위

---
+++ class/src/array/Ex14_array.java
@@ -0,0 +1,17 @@
+package array;
+
+
+public class Ex14_array {
+
+	public static void main(String[] args) {
+		int[] arr=new int[10];
+		System.out.println("arr.length="+arr.length);
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("arr["+i+"]="+arr[i]);
+		}
+		
+
+	}
+
+}
