e6c1361a0b9f1e28122943091701d10184c3e594
yh <yh@DESKTOP-AD3VHGH>
2020-12-06 19:35:24 +0900

    배열의 출력

---
+++ class/src/array/Ex5_6.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Ex5_6 {
+
+	public static void main(String[] args) {
+		
+		int[] iArr= {100,95,80,70,60}; //길이가 5인 int배열
+		for(int i=0;i<iArr.length;i++) {
+			System.out.println(iArr[i]);
+			
+		}
+	
+	}
+
+}
