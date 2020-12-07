046420f7bd681c13f8efbc1f4cf3bec6eccde9fb
yh <yh@DESKTOP-AD3VHGH>
2020-12-07 17:18:29 +0900

    Arrays로 배열

--- class/src/array/Ex5_1.java
+++ class/src/array/Ex5_1.java
@@ -4,8 +4,9 @@ public class Ex5_1 {
 
 	public static void main(String[] args) {
 		int sum=0;
-		float avg =0f;
-		int[] score = {100,88,100,90,100};
+		float avg=0;
+		
+		int[] score= {100,90,88,100,100};
 		for(int i=0;i<score.length;i++) {
 			sum+=score[i];
 		}
