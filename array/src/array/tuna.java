262d80577191c2d3b2bfcb59cf5d52e54ee3b3c5
yh <yh@DESKTOP-AD3VHGH>
2020-12-08 11:49:25 +0900

    Use Methods with Parameters

--- array/src/array/tuna.java
+++ array/src/array/tuna.java
@@ -1,9 +1,9 @@
 package array;
 
 public class tuna {
-	public void simpleMessage() {
-		System.out.println("This is another class");
+	public void simpleMessage(String name) {
+		System.out.println("I am sure you can do well  "+name);
 	}
 	
-	
+
 }
