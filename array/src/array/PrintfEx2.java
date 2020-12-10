b7c055a81534997501b542630a2cfbcab9abe1ed
yh <yh@DESKTOP-AD3VHGH>
2020-12-10 20:13:29 +0900

    printf의 지시자

--- array/src/array/PrintfEx2.java
+++ array/src/array/PrintfEx2.java
@@ -2,9 +2,14 @@ package array;
 
 public class PrintfEx2 {
 	public static void main(String[] args) {
-	System.out.printf("%d\n",15);
-	System.out.printf("%o\n",15); // 8진수
-	System.out.printf("%x\n",15); //16진수
-	System.out.printf("%s",Integer.toBinaryString(15));
-}
+//	System.out.printf("%d\n",15);
+//	System.out.printf("%o\n",15); // 8진수
+//	System.out.printf("%x\n",15); //16진수
+//	System.out.printf("%s",Integer.toBinaryString(15));
+
+	float f =123.456789f;
+	System.out.printf("%f\n",f);
+	System.out.printf("%e\n",f);
+	
+	}
 }
