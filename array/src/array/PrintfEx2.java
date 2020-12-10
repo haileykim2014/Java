62973e1ddf1d641aea420aafa2eeccb1e87ca6be
yh <yh@DESKTOP-AD3VHGH>
2020-12-10 20:11:36 +0900

    printf를 이용한 출력

---
+++ array/src/array/PrintfEx2.java
@@ -0,0 +1,10 @@
+package array;
+
+public class PrintfEx2 {
+	public static void main(String[] args) {
+	System.out.printf("%d\n",15);
+	System.out.printf("%o\n",15); // 8진수
+	System.out.printf("%x\n",15); //16진수
+	System.out.printf("%s",Integer.toBinaryString(15));
+}
+}
