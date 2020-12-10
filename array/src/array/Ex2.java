fb99fbbfc56a3e263f0f672131c85d0647b31cdc
yh <yh@DESKTOP-AD3VHGH>
2020-12-10 23:34:19 +0900

    타입간의 변환방법

---
+++ array/src/array/Ex2.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		String str ="3";
+		System.out.println(str.charAt(0)-'0'); // 3 ¼ıÀÚ
+		System.out.println(str.charAt(0)+1); //52
+		System.out.println(Integer.parseInt("3")+1);
+		System.out.println("3"+1);//31
+		
+	}
+	
+	
+}
