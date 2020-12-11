5f64e118f84b743920210943cafe60734cb03a26
yh <yh@DESKTOP-AD3VHGH>
2020-12-11 17:02:06 +0900

    성적표 else if

--- array/src/array/Ex4.java
+++ array/src/array/Ex4.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class Ex4 {
 
 	public static void main(String[] args) {
-		int score=0; // 점수저장하기위한 변수
-		char grade='D'; //학점을 저장하기위한 변수. 공백으로 초기화한다
+		int score=0;
+		char grade='c', opt='0';
+		System.out.println("점수를 입력해주세요");
+		Scanner scanner= new Scanner(System.in);
+				score=scanner.nextInt();
 		
-		System.out.println("점수를 입력하셈");
-		Scanner scanner = new Scanner(System.in);
-		score=scanner.nextInt(); //화면을 통해 입력받은 숫자를 score에 저장
+		System.out.printf("너의점수는 %d입니다.",score);
 		
-		if(score>=90) { //score가 90보다 같거나 크면 A학점
+		if(score>=90) {
 			grade='A';
-		}else if(score>=80) {
-			grade='B';
-		}else if(score>=70) {
-			grade='C';
-//	}else {
-//		grade='D';
-	} System.out.println("네 학점은"+grade+"이야");
-}
+			if(score>=98) {
+				opt='+';
+			} else if(score<94) {
+				opt='-';
+			}}
+		else if(score>=88) {
+				grade='B';
+				if(score>=88) {
+					opt='+';
+			}else if(score<84) {
+				opt='-';
+			}
+	    } 
+		System.out.printf("너의학점은%c%c입니다.",grade,opt);
+	}
 }
