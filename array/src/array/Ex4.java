8d9da76275c95d6d719f91756b3b253a4aca3730
yh <yh@DESKTOP-AD3VHGH>
2020-12-11 14:45:25 +0900

    if else 예제

---
+++ array/src/array/Ex4.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Scanner;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		int score =0;
+		char grade =' ';
+		
+		System.out.println("점수를 입력하셈");
+		Scanner scanner = new Scanner(System.in);
+		score = scanner.nextInt();
+		
+		if(score>=90) {
+			grade='A';
+		} else if(score>=80) {
+			grade='B';
+		} else if(score>=70) {
+			grade='C';
+		}else {
+			grade='D';
+		}System.out.println("니 학점은"+grade+"이야");
+	}
+}
