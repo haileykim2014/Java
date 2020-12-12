1c1ebf81f5799ec6c4ebaa1336e8866f8b93f03e
yh <yh@DESKTOP-AD3VHGH>
2020-12-12 19:06:27 +0900

    for 문에 변수 두개사용

--- array/src/array/Ex4_8.java
+++ array/src/array/Ex4_8.java
@@ -3,8 +3,8 @@ package array;
 public class Ex4_8 {
 
 	public static void main(String[] args) {
-		for (int i=1;i<=10; i=i+2) {//i=1,3,5,7,9
-			System.out.println("hii");
+		for (int i=1,j=10;i<=10; i++,j--) {//i=1,3,5,7,9
+			System.out.println("i="+i+",j="+j);
 		}
 
 	}
