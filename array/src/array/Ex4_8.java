a5811cc257dda8e98cc0cb1094786c67beb77bc0
yh <yh@DESKTOP-AD3VHGH>
2020-12-12 18:59:52 +0900

    for 반복문

--- array/src/array/Ex4_8.java
+++ array/src/array/Ex4_8.java
@@ -3,7 +3,7 @@ package array;
 public class Ex4_8 {
 
 	public static void main(String[] args) {
-		for (int i=1;i<=3; i++) {
+		for (int i=1;i<=10; i=i+2) {//i=1,3,5,7,9
 			System.out.println("hii");
 		}
 
