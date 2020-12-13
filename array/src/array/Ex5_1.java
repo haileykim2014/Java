56eef96625a2da5d1bcc618d91e57719e8110055
yh <yh@DESKTOP-AD3VHGH>
2020-12-13 23:21:53 +0900

    인덱스 범위

--- array/src/array/Ex5_1.java
+++ array/src/array/Ex5_1.java
@@ -3,9 +3,12 @@ package array;
 public class Ex5_1 {
 
 	public static void main(String[] args) {
-		int[] arr = new int[5];
+		int[] arr = new int[10];
 		System.out.println(arr.length);
 		
+		for(int i=0;i<10;i++) {
+			System.out.println("arr["+i+"]="+arr[i]);
+		}
 	}
 
 }
