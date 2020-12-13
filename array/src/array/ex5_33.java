57fd864e25667d8ba1c116b1c0eaf7c96dcb4cb9
yh <yh@DESKTOP-AD3VHGH>
2020-12-13 23:54:53 +0900

    배열의 출력

---
+++ array/src/array/ex5_33.java
@@ -0,0 +1,15 @@
+package array;
+
+public class ex5_33 {
+
+	public static void main(String[] args) {
+	
+		int[] iArr = {100,95,80,70,60};
+		for(int i=0; i<iArr.length; i++) {
+			   System.out.println(iArr[i]);  //배열의 요소를 순서대로 하나씩출력
+
+
+	}
+
+}
+}
