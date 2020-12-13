84278bacaff4cf09b899aa3d67e2860d75f0a2f0
yh <yh@DESKTOP-AD3VHGH>
2020-12-13 23:03:30 +0900

    배열의 생성과 선언

---
+++ array/src/array/Ex5_11.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Ex5_11 {
+
+	public static void main(String[] args) {
+		// int[] score; 1. 배열 score를 선언
+		// score = new int[5]; 2.배열의 생성
+	
+	 int[] score = new int[5];
+	 score[3] = 100;
+	 
+	 System.out.println(score[3]);
+	}
+
+}
