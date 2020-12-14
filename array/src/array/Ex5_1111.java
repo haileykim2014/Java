fb8a2b545e5066b2c5bd50ad7c83757f0d157b4b
Haileykim <haileykim2014@gmail.com>
2020-12-14 23:59:11 +0900

    String 클래스 주요 메서드

--- array/src/array/Ex5_1111.java
+++ array/src/array/Ex5_1111.java
@@ -5,8 +5,10 @@ public class Ex5_1111 {
 	public static void main(String[] args) {
 		//           01234
 		String str ="ABCDE";
-		char ch = str.charAt(2);
+		char ch = str.charAt(4);
 		System.out.println(ch);
+		String str2 = str.substring(1,4);
+		System.out.println(str2);
 	}
 
 }
