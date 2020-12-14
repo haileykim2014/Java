1fa5de0837b7df88ebc4e6f0bff7fcf36aaeb230
Haileykim <haileykim2014@gmail.com>
2020-12-14 23:51:04 +0900

    String 클래스

---
+++ array/src/array/Ex5_1111.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Ex5_1111 {
+
+	public static void main(String[] args) {
+		//           01234
+		String str ="ABCDE";
+		char ch = str.charAt(2);
+		System.out.println(ch);
+	}
+
+}
