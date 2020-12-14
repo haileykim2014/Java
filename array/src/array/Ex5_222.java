226571273432d33b97ded390799e9029f50addb5
yh <yh@DESKTOP-AD3VHGH>
2020-12-14 20:37:37 +0900

    배열 총합 평균

--- array/src/array/Ex5_222.java
+++ array/src/array/Ex5_222.java
@@ -1,10 +1,10 @@
 package array;
 
-public class Ex5_2 {
+public class Ex5_222 {
 
 	public static void main(String[] args) {
-		int sum =0;
-		float average = 0;
+		int sum =0; //총점을 저장하기 위한 변수
+		float average = 0; // 평균을 저장하기 위한 변수
 		
 		int[] score= {100,88,100,100,90};
 		
