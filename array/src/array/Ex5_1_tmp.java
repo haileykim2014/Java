ac6a953a43a33d2a46513214af4c1ab8cea515d9
Haileykim <haileykim2014@gmail.com>
2020-12-15 01:06:49 +0900

    Arrays 배열

--- array/src/array/Ex5_1_tmp.java
+++ array/src/array/Ex5_1_tmp.java
@@ -10,8 +10,25 @@ public class Ex5_1_tmp {
 		
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.deepToString(arr2D));
-
 		
+		String[][] str2D = {{"aaa","bbb"},{"AAA","BBB"}};
+		String[][] str2D2 = {{"aaa","bbb"},{"AAA","BBB"}};
+		
+		//문자열비교시 등가식 불가 
+		//System.out.println(str2D==str2D2); 
+		//위 식은 참조변수 값비교이다. 
+		// 서로 다른 배열이기 때문에 같은 메모리상에 있지않다. 절대 겹칠수 없다(항상 false)
+		
+		System.out.println(Arrays.deepEquals(str2D,str2D2));
+		
+		int[] arr2 = Arrays.copyOf(arr,arr.length);
+		/* 배열의 복사
+		   배열arr을 복사해서 arr2를 만든다. arr.lenth만큼복사
+		*/
+		System.out.println(Arrays.toString(arr2));
+		
+		int[] arr3 = Arrays.copyOfRange(arr,2,4); //
+		System.out.println(Arrays.toString(arr3));
 	}
 
 }
