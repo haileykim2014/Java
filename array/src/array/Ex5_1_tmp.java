c10006fd07282f819de483528b25c001853b0f5a
Haileykim <haileykim2014@gmail.com>
2020-12-15 00:59:34 +0900

    Arrays 배열

---
+++ array/src/array/Ex5_1_tmp.java
@@ -0,0 +1,17 @@
+package array;
+
+import java.util.Arrays;
+
+public class Ex5_1_tmp {
+
+	public static void main(String[] args) {
+		int[] arr = {0,1,2,3,4}; //1차원배열 arr
+		int[][] arr2D = {{11,12},{21,22}};//2차원배열
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.deepToString(arr2D));
+
+		
+	}
+
+}
