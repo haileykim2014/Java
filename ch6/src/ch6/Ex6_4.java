04dc0645a2133704f6808a139759b5333681e14c
Haileykim <haileykim2014@gmail.com>
2020-12-16 00:21:17 +0900

    메서드 호출

--- ch6/src/ch6/Ex6_4.java
+++ ch6/src/ch6/Ex6_4.java
@@ -3,8 +3,8 @@ package ch6;
 public class Ex6_4 {
 
 	public static void main(String[] args) {
-		MyMath mm = new MyMath();
-		long result1 = mm.add(5L,3L);
+		MyMath mm = new MyMath(); //객체생성
+		long result1 = mm.add(5L,3L); //add 메서드 호출
 		long result2 = mm.subtract(5L,3L);
 		long result3 = mm.multiply(5L,3L);
 		double result4 = mm.divide(5L,3L);
@@ -21,7 +21,7 @@ public class Ex6_4 {
 class MyMath {
 	long add(long a, long b) {
 		 long result = a+b;
-		 return result;
+		 return result; //작업을 마치면 호출한곳으로 돌아온다.  result1=8
 		 // return a+b; // 위의 두줄을 이와같이 한줄로 간단히가능
 	}
 	long subtract(long a, long b) { return a-b;}
