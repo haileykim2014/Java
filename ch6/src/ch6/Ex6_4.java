68a4cf62297eccd144f856ba896aadfb6661f5c0
Haileykim <haileykim2014@gmail.com>
2020-12-16 00:44:16 +0900

    return

--- ch6/src/ch6/Ex6_4.java
+++ ch6/src/ch6/Ex6_4.java
@@ -8,6 +8,7 @@ public class Ex6_4 {
 		long result2 = mm.subtract(5L,3L);
 		long result3 = mm.multiply(5L,3L);
 		double result4 = mm.divide(5L,3L);
+		mm.printGugudan(3); //구구단 3단을 출력
 		
 		System.out.println("add(5L,3L)="+result1);
 		System.out.println("subtract(5L,3L)="+result2);
@@ -19,6 +20,15 @@ public class Ex6_4 {
 }
 
 class MyMath {
+	void printGugudan(int dan) {
+		if(!(2<=dan&&dan<=9))
+			return; //입력받은 단이 2~9단이 아니면, 메서드 종료하고 돌아가기
+		for(int i=1;i<=9;i++) {
+			System.out.printf("%d * %d = %d%n",dan,i,dan*i);
+		
+		
+		}
+	}
 	long add(long a, long b) {
 		 long result = a+b;
 		 return result; //작업을 마치면 호출한곳으로 돌아온다.  result1=8
