2b53fab38961da348d9379b2d1c77e8675dd696a
Haileykim <haileykim2014@gmail.com>
2020-12-17 23:57:35 +0900

    6.6필드

---
+++ ch6/src/sec06/Car.java
@@ -0,0 +1,11 @@
+package sec06;
+
+public class Car {
+	String company = "현대자동차"; //필드선언 및 초기값 줌
+	String model = "그랜저"; //필드선언 및 초기값 줌
+	String color = "검정"; //필드선언 및 초기값 줌
+	int maxSpeed = 350; //필드선언 및 초기값 줌
+	int speed; //기본 0값
+	//자바에서는 변수가 아니라 필드라고 부른다.
+	
+}
