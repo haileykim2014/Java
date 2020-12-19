0e0cf18d80ebfac3ff114d038a7973905ec861a4
Haileykim <haileykim2014@gmail.com>
2020-12-19 18:33:14 +0900

    상속과 포함 참조변수의 초기화

--- ch6/src/ch6/Inheritance.java
+++ ch6/src/ch6/Inheritance.java
@@ -4,19 +4,24 @@ class Point {
 	int x;
 	int y;
 }
-class Circle extends Point {//상속
+//class Circle extends Point {//상속
+//	int r;
+//}
+class Circle { //포함
+	Point p = new Point(); //참조변수의 초기화
 	int r;
+	
 }
 
 public class Inheritance {
 	public static void main(String[] args) {
 		Circle c= new Circle();
-		c.x = 1;
-		c.y = 2;
+		c.p.x = 1;
+		c.p.y = 2;
 		c.r = 3;
 		
-		System.out.println("c.x="+c.x);
-		System.out.println("c.y="+c.y);
+		System.out.println("c.x="+c.p.x);
+		System.out.println("c.y="+c.p.y);
 		System.out.println("c.r="+c.r);
 		
 	}
