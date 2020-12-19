134e3b117c70dbc1a542bca176c4cc847b52cb04
Haileykim <haileykim2014@gmail.com>
2020-12-19 20:55:59 +0900

    오버로딩

--- ch6/src/ch6/Override.java
+++ ch6/src/ch6/Override.java
@@ -1,30 +1,31 @@
 package ch6;
 
-class MyPoint3{
+class MyPoint3 extends Object{
 	int x;
 	int y;
-	String getLocation() {
-		return "x:"+x+",y:"+y;
-		}
-}
-class MyPoint3D extends MyPoint3 {
-	int z;
-	//조상의 getLocation()을 오버라이딩
-	String getLocation() {
-		return "x:"+x+",y:"+y+",z:"+z;
-		}	
+	
+	MyPoint3(int x, int y){ //생성
+		this.x=x;
+		this.y=y;
+		
+	}
+
+	//object클래스의 toString()을 오버라이딩
+	public String toString() {
+		return"x:"+x+",y:"+y;
+	}
 }
 
 public class Override {
-
 	public static void main(String[] args) {
-		MyPoint3D p =new MyPoint3D();
+		MyPoint3 p =new MyPoint3(3,5);
+		//	MyPoint3 p =new MyPoint3(0); 한번에초기화
 		p.x=3;
 		p.y=5;
-		p.z=7;
-		System.out.println(p.getLocation());
-		
+//		System.out.println("p.x="+p.x);
+//		System.out.println("p.x="+p.x);
+//		System.out.println(p.toString());
+		System.out.println(p); //참조변수로 toString이 출력된다.
 
 	}
-
 }
