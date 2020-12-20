054273d542541fb11ff8b853cfe1fcd71d71e3d5
Haileykim <haileykim2014@gmail.com>
2020-12-20 21:04:23 +0900

    접근제어자

--- ch6/src/ch7/MyParantTest.java
+++ ch6/src/ch7/MyParantTest.java
@@ -1,30 +1,28 @@
 package ch7;
 
-class MyParant{
-	private int prv; //같은클래스
-			int dft; //같은패키지
-	protected int prt; //같은패키지 +자손
-	public  int pub; //접근제한없음
+class MyParent{
+	private		 int prv; //같은클래스
+				 int dft; //같은클래스
+	protected	 int prt; //같은클래스+자손(다른패키지)
+	public 		 int pub; //접근제한없음
 	
-	public void printMembers() {
-		System.out.println(prv);//ok
-		System.out.println(dft);//ok
-		System.out.println(prt);//ok
-		System.out.println(pub);//ok
+	public void printmembers() {
+		System.out.println(prv); //ok
+		System.out.println(dft); //ok
+		System.out.println(prt); //ok
+		System.out.println(pub); //ok
 	}
-
 }
 
 public class MyParantTest {
 
 	public static void main(String[] args) {
-	  MyParant p = new MyParant();
-	  System.out.println(p.prv); //에러
-	  System.out.println(p.dft); //ok
-	  System.out.println(p.prt); //ok
-	  System.out.println(p.pub); //ok
-	  
-
+		MyParent p = new MyParent();
+	
+		System.out.println(p.prv);//오류
+		System.out.println(p.dft);//ok
+		System.out.println(p.prt);//ok
+		System.out.println(p.pub);//ok
 	}
 
 }
