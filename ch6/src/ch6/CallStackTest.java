d0fc086fac6134e46e3777b27b4cb69d3f11d670
Haileykim <haileykim2014@gmail.com>
2020-12-21 16:31:59 +0900

    호출스택

---
+++ ch6/src/ch6/CallStackTest.java
@@ -0,0 +1,14 @@
+package ch6;
+
+public class CallStackTest {
+	public static void main(String[] args) {
+		firstMethod(); //static메서드는 객체생성없이 호출가능하다
+		
+	}
+	static void firstMethod() {
+		secondMethod();
+	}
+	static void secondMethod() {
+		System.out.println("secondMethod");
+	}
+}
