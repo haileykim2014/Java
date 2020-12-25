35393c00c4a041d17acdc8d3065c270f57d9201a
Haileykim <haileykim2014@gmail.com>
2020-12-25 23:48:41 +0900

    인터페이스구현

---
+++ ch6/src/sec03_multi/RemoteControlExam.java
@@ -0,0 +1,10 @@
+package sec03_multi;
+
+public class RemoteControlExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ch6/src/sec03_multi/Searchable.java
@@ -0,0 +1,5 @@
+package sec03_multi;
+
+public interface Searchable {
+
+}
