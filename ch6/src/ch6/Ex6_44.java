ad88a758d74d8c5eb831dc256656e0f4072765fc
Haileykim <haileykim2014@gmail.com>
2020-12-27 15:13:10 +0900

    메서드호출

--- ch6/src/ch6/Ex6_44.java
+++ ch6/src/ch6/Ex6_44.java
@@ -8,21 +8,29 @@ public class Ex6_44 {
 		long result2 = mm.subrtact(5L,3L);
 		long result3 = mm.multiply(5L,3L);
 		double result4 = mm.divide(5L,3L);
+		long result5 = mm.max(5, 3);
 		
 		System.out.println(result1);
 		System.out.println(result2);
 		System.out.println(result3);
 		System.out.println(result4);
+		System.out.println(result4);
+		System.out.println(result5);
 	}
 
 }
 
-class MyJava{
+class MyJava{ //메서드는 클래스영역에만 정의가능
 	long add(long a, long b) {
 		long result = a+b;
 		return result;
 		//return a+b; //위의 두줄을 이와같이 한줄로 간단히 할 수있다.
 	}
+	long max(long a, long b) { 
+		
+		return a > b ? a : b;
+		}
+	
 	long subrtact(long a, long b) {return a-b;}
 	long multiply(long a, long b) {return a*b;}
 	double divide(double a , double b) {
