52f20741da9d2d78661d58eef9af7bbcb60f29ce
Haileykim <haileykim2014@gmail.com>
2020-12-28 20:11:01 +0900

    배열예외처리
    if와 else를 추가해서 오류발생시 배열의길이확인문구 및 안내적용

--- ch6/src/ch8/ArrayIndexOufOfBounds.java
+++ ch6/src/ch8/ArrayIndexOufOfBounds.java
@@ -3,12 +3,17 @@ package ch8;
 public class ArrayIndexOufOfBounds {
 
 	public static void main(String[] args) {
+		if(args.length==2){//배열의 길이2 이라면
 		String data1 = args[0];
 		String data2 = args[1];
 		
 		System.out.println(data1);
 		System.out.println(data2);
-
 	}
-
+		else {
+			System.out.println("[실행방법]");
+			System.out.println("java ArrayIndexOufOfBounds");
+			System.out.println("값1 값2");
+		}
+}
 }
