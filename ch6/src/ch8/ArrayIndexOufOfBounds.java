e583de7614d9d8fe512e37acb251617fee58bc96
Haileykim <haileykim2014@gmail.com>
2020-12-28 20:08:15 +0900

    실행 예외 배열예외처리

---
+++ ch6/src/ch8/ArrayIndexOufOfBounds.java
@@ -0,0 +1,14 @@
+package ch8;
+
+public class ArrayIndexOufOfBounds {
+
+	public static void main(String[] args) {
+		String data1 = args[0];
+		String data2 = args[1];
+		
+		System.out.println(data1);
+		System.out.println(data2);
+
+	}
+
+}
