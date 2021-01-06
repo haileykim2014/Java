abbeffbcd9ca91e8b42431b2b78a06a46b937d25
Haileykim <haileykim2014@gmail.com>
2021-01-07 01:44:29 +0900

    형변환

--- ch6/src/Ex3_11.java
+++ ch6/src/Ex3_11.java
@@ -8,7 +8,11 @@ public class Ex3_11 {
 		System.out.println(pi*1000);
 		System.out.println(Math.round(pi*1000));
 		System.out.println(Math.round(pi*1000)/1000.0);
-
+		
+		//3.141 ?
+		//3141
+		System.out.println(pi*1000);
+		System.out.println((int)(pi*1000)/1000.0);
 	}
 
 }
