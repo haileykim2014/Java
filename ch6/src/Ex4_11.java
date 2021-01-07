571308a7b55adf79f7742bf07741e29526617f39
Haileykim <haileykim2014@gmail.com>
2021-01-07 15:08:10 +0900

    i와 j가 동시에 변함

--- ch6/src/Ex4_11.java
+++ ch6/src/Ex4_11.java
@@ -2,13 +2,18 @@
 public class Ex4_11 {
 
 	public static void main(String[] args) {
-		for(int i=2; i<=9; i++) {
-			for(int j=1;j<=9;j++) {
-				System.out.println(i+"*"+j+"="+(i*j));
-			}
-			System.out.println();
-		}
+		
+		for(int i=1;i<=5;i++) { //5번 반복
+			for(int j=1;j<=i;j++) 
+		 {
+			System.out.print("*");
+				}
+			
+		System.out.println();
 
 	}
-
+	}
+	
 }
+
+
