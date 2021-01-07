0556599adfdb65612617f459af93fce84b8aa2cb
Haileykim <haileykim2014@gmail.com>
2021-01-07 22:28:09 +0900

    배열의길이

--- ch6/src/Ex5_1.java
+++ ch6/src/Ex5_1.java
@@ -2,21 +2,12 @@
 public class Ex5_1 {
 
 	public static void main(String[] args) {
-//		int[] score;
-//		score = new int[5];
+		int[] arr = new int[5];
+		System.out.println("arr.length="+arr.length);
 		
-		int[] score = new int[5];
-		score[3] = 100;
-		
-		System.out.println("score[0]="+score[0]);
-		System.out.println("score[1]="+score[1]);
-		System.out.println("score[2]="+score[2]);
-		System.out.println("score[3]="+score[3]);
-		System.out.println("score[4]="+score[4]);
-
-		int value = score[3];
-		System.out.println(value);
-
+		for(int i=0;i<10;i++) {
+			System.out.println("arr["+i+"]="+arr[i]);
+		}
 	}
-
 }
+
