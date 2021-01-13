6473878123f3ca7c38c55def51058f7857f1bcf6
Haileykim <haileykim2014@gmail.com>
2021-01-14 00:55:55 +0900

    내부클래스

--- ch6/src/sec03/exam01/InnerTest.java
+++ ch6/src/sec03/exam01/InnerTest.java
@@ -1,28 +1,28 @@
 package sec03.exam01;
 
-class A{
+class A{ //A는 B의 외부클래스
 	int i=100;
 	B b = new B();
-}
-
-
-class B{
-	void method() {
-		A a =new A();
-		System.out.println(a.i);
+	
+	class B{ //B는 A의 내부클래스
+		void method() {
+//			A a =new A();
+//			System.out.println(a.i);
+			System.out.println(i);//객체생성없이 외부클래스의 멤버접근가능
+		}
 	}
 }
 
-class C{
-	B b =new B();
-}
+//class C{
+//	B b =new B();
+//}
 
 
 public class InnerTest {
 
 	public static void main(String[] args) {
-		B b = new B();
-		b.method();
+//		B b = new B();
+//		b.method();
 
 	}
 
