75dcc47cbdfca4840e99b7228cdf74516508db4c
Haileykim <haileykim2014@gmail.com>
2021-01-17 00:45:24 +0900

    반복문 for,boolean 연습문제

---
+++ ch6/src/ch9/Practice4_13.java
@@ -0,0 +1,13 @@
+package ch9;
+
+public class Practice4_7 {
+	public static void main(String[] args) {
+
+		for(int i=1;i<=6;i++) {
+			System.out.println((int)(Math.random()*6)+1);
+		}
+	}
+	
+}
+
+
