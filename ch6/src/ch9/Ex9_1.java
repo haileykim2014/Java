51bd96c702c7b8d4a229a07e1a3d9c9b665904f5
Haileykim <haileykim2014@gmail.com>
2021-01-19 18:14:50 +0900

    equals의 오버라이딩

--- ch6/src/ch9/Ex9_1.java
+++ ch6/src/ch9/Ex9_1.java
@@ -9,21 +9,21 @@ public class Ex9_1 {
 		Value v2 = new Value(10);
 		
 		if(v1.equals(v2))
-			System.out.println("v1과 v1는 같습니다");
-		else
-			System.out.println("v1과 v2는 다릅니다");
-
-	} //main
-
+			System.out.println("v1과 v2는 같습니다.");
+		else System.out.println("v1과 v2는 다릅니다.");
+	}
 }
+
 class Value{
 	int value;
-	Value(int Value){
+	Value(int value){
 		this.value = value;
 	}
-	public boolean equals(Object obj) {
-		Value v = (Value)obj;
-		
-		return this.value==v.value;
+	public boolean equals(Object obj) { // 오버라이딩
+		Value v = (Value)obj; // obj를 value로 형변환(다형성)
+		//참조변수의 형변환전에는 반드시 instanceof로 확인해야함.
+		if(!(obj instanceof Value)) return false;
+		return this.value==v.value; //값이 같은지 ?
+//		return this.value==obj.value; //주소비교.서로 다른객체는 항상거짓
 	}
 }
