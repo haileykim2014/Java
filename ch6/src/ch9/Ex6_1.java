d9e6486566a1e913d2ead4b468f131085804b3e4
Haileykim <haileykim2014@gmail.com>
2021-01-22 22:53:01 +0900

    객체지향 인스턴스의 생성과 사용

---
+++ ch6/src/ch9/Ex6_1.java
@@ -0,0 +1,26 @@
+package ch9;
+
+public class Ex6_1 {
+
+	public static void main(String[] args) {
+	Tv t;
+	t = new Tv();
+	t.channel = 7;
+	t.channelUp();
+	System.out.println("현재채널은" + t.channel + "입니다");
+	}
+
+}
+
+class Tv{
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() { power = !power;}
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+		
+}
+
+
