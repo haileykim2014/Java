0d5d8bfd74b65591adfcda9789a67cfcd3d254ed
Haileykim <haileykim2014@gmail.com>
2021-01-24 19:55:44 +0900

    Vector클래스
    List<E>list=new Vector<E>();
    벡터는 스레드동기화가 되어있다.
    스레드에 안전하다

---
+++ ch6/src/ch11/VectorEx.java
@@ -0,0 +1,27 @@
+package ch11;
+
+import java.util.*;
+
+public class VectorEx {
+
+	public static void main(String[] args) {
+		List<Board>list = new Vector<Board>();
+		
+		list.add(new Board("제목1","내용1","글쓴이1"));
+		list.add(new Board("제목2","내용2","글쓴이2"));
+		list.add(new Board("제목3","내용3","글쓴이3"));
+		list.add(new Board("제목4","내용4","글쓴이4"));
+		list.add(new Board("제목5","내용5","글쓴이5"));
+		
+		list.remove(2); //2번인덱스제거
+		list.remove(3); //리뉴얼된 3번인덱스제거 : 제목5제거됨
+		
+		for(int i=0;i<list.size();i++) {
+			Board board = list.get(i);
+			System.out.println(board.subject+"\t"+board.content+"\t"+board.writer);
+		}
+//		for(Board board:list){ // 향상된for문과 동일한내용
+//	}
+	}
+
+}
