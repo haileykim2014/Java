f87a6d7afba03e4e94ffde893a6ce430bba4a7d4
Haileykim <haileykim2014@gmail.com>
2021-01-24 22:03:38 +0900

    Set 컬렉션

--- ch6/src/ch11/HashSetEx.java
+++ ch6/src/ch11/HashSetEx.java
@@ -1,7 +1,6 @@
 package ch11;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 public class HashSetEx {
@@ -12,11 +11,20 @@ public class HashSetEx {
 		set.add("Java");
 		set.add("JDBC");
 		set.add("Servlet");
-		set.add("Java");
+		set.add("Java"); // 이미 동일한 문자가 첫번째에 저장되어있음.한번만 저장됨
 		set.add("iBATIS");
 		
 		int size = set.size();
 		System.out.println("총 객체수: "+size);
+		
+		Iterator<String>iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String element = iterator.next();
+			System.out.println("\t"+element);
+		}
+		set.remove("JDBC");
+		set.remove("iBATIS");
+		System.out.println("총 객체수: "+set.size());
 	}
 
 }
