c5a87acdf38aff931db8dce37af0e2e27bfe4407
Haileykim <haileykim2014@gmail.com>
2021-01-24 19:27:34 +0900

    List컬렉션
    list.add();//값저장
    int size =list.size(); //총객체수
    String skill = list.get(2);//인덱스2번째 값가져와
    list.remove(2);//2번째인덱스값지우고 3번째가 2번째로당겨짐

--- ch6/src/ch11/ArrayListEx.java
+++ ch6/src/ch11/ArrayListEx.java
@@ -15,7 +15,27 @@ public class ArrayListEx {
 		
 		int size = list.size();
 		System.out.println("총 객체수: "+size);
-
+		System.out.println();
+		
+		String skill = list.get(2);//인덱스2위치에있는것 가져와
+		System.out.println(skill);
+		System.out.println();
+		
+		for(int i=0;i<list.size();i++) {
+			String str = list.get(i);
+			System.out.println(i+":"+str);
+		}
+		System.out.println();
+		list.remove(2); //2번인덱스객체가 삭제되고 3번이 2번으로 4번이3번으로 당겨짐
+		list.remove("iBATIS");//문자삭제
+		
+		for(int i=0;i<list.size();i++) {
+			String str = list.get(i);
+			System.out.println(i+":"+str);
+		}
+		System.out.println();
+		
+		
 	}
 
 }
