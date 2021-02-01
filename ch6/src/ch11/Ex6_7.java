0d37afb70598755a8d3654fff0e3493fde709cae
Haileykim <haileykim2014@gmail.com>
2021-02-02 00:45:03 +0900

    static 메소드와 인스턴스메소드

--- ch6/src/ch11/Ex6_7.java
+++ ch6/src/ch11/Ex6_7.java
@@ -1,35 +1,24 @@
 package ch11;
 
-import java.util.*;
-
-public class Ex6_4 {
-
+public class Ex6_7 {
 	public static void main(String[] args) {
-		System.out.println("Card.width="+Card.width);
-		System.out.println("Card.height="+Card.height);
-		
-		Card c1 = new Card(); //인스턴스 생성
-		c1.kind = "Heart";
-		c1.number = 7;
-		
-		Card c2 = new Card();
-		c2.kind = "Space";
-		c2.number = 4;
-		
-		System.out.println("c1은"+c1.kind+c1.number+"크기는"+c1.width+","+c1.height);
-		System.out.println("c2은"+c2.kind+c2.number+"크기는"+c2.width+","+c2.height);
-		System.out.println("c1의 width와 height를 각가 50,80으로 변경합니다");
+		MyPoint p = new MyPoint(1,1);
+		//p와 (2,2)의 거리를 구한다.
+		System.out.println(p.getDistance(2,2));
 
-		Card.width = 50; //클래스변수는 공유하기때문에 같이바뀐다.
-		Card.height = 80; //클래스변수 사용시 Card.width와 같이 클래스이름.클래스변수로 사용할것
-		System.out.println("c1은"+c1.kind+c1.number+"크기는"+c1.width+","+c1.height);
-		System.out.println("c2은"+c2.kind+c2.number+"크기는"+c2.width+","+c2.height);
 	}
 }
-class Card{
-	String kind; 
-	int number;
-	static int width = 100; //클래스변수
-	static int height = 250; //클래스변수
+class MyPoint{
+	int x;
+	int y;
+	
+	MyPoint(int x, int y){
+		this.x= x;
+		this.y= y;
+	}
+	double getDistance(int x1,int y1){
+		return Math.sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1));
+	}
 	
 }
+
