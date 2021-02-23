fd54d8826500d8ee4e3db69597fd953c031bd386
Haileykim <haileykim2014@gmail.com>
2021-02-23 23:08:32 +0900

    클래스정의 및 인스턴스생성

---
+++ jungsuk/src/jungsuk/BankAccountOO.java
@@ -0,0 +1,37 @@
+package jungsuk;
+
+ class Bankaccountt{
+	    int balance = 0;     // ¿¹±Ý ÀÜ¾×
+
+	    public int deposit(int amount) {
+	        balance += amount;
+	        return balance;
+	    }    
+
+	    public int withdraw(int amount) {
+	        balance -= amount;
+	        return balance;
+	    }
+
+	    public int checkMyBalance() {
+	        System.out.println("ÀÜ¾× : " + balance);
+	        return balance;
+	    }
+	}
+		
+
+public	class BankAccountOO {
+	    public static void main(String[] args) {
+	    	Bankaccountt yoon = new Bankaccountt();
+	    	Bankaccountt park = new Bankaccountt();
+
+	        yoon.deposit(5000);
+	        park.deposit(3000);
+
+	        yoon.withdraw(2000);
+	        park.withdraw(2000);
+
+	        yoon.checkMyBalance();
+	        park.checkMyBalance();  
+	    }
+	}
