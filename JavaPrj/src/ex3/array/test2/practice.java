eb5b5bf94568245499cd19cf0a54d9e9a6a2dc0d
Haileykim <haileykim2014@gmail.com>
2021-03-11 14:30:00 +0900

    문자열배열, 정렬

--- JavaPrj/src/ex3/array/test2/practice.java
+++ JavaPrj/src/ex3/array/test2/practice.java
@@ -1,96 +1,51 @@
 package ex3.array.test2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class practice {
 
-	public static void main(String[] args) {
-		
-		//1.정수 10개짜리 nums배열을 생성한다. 단, 1,2,3,4~10
+	public static void main(String[] args) throws IOException {
+	
+		String[] names = new String[] {"강호동","유재석","하하","김지호","김현준","김태희","박민정"};
 
-		int[] nums = {10,2,9,1,4,6,8,3,5,7};
+		Random rand = new Random();
 		
+		for(int i=0; i<100;i++) {
+			int idx1 = rand.nextInt(7); //0~6
+			int idx2 = rand.nextInt(7); //0~6
+//			System.out.printf("idx1 :%d, idx2:%d\n",idx1,idx2); //랜덤값을 확인한다.
+						
+			String temp; //데이터형이달라져도 똑같다.
+			temp = names[idx1]; //랜덤값끼리 섞어준다
+			names[idx1] = names[idx2];
+			names[idx2] = temp;			
+		}
+		
+		for(int i=0;i<7;i++) {
+			System.out.printf("%s",names[i]);
+			if(i<7-1)
+				System.out.print(",");		
+		}System.out.println();
+		//정렬
 
-			for(int i=0;i<9;i++)
-				if(nums[i]>nums[i+1]) {
-					int temp;
-					temp = nums[i];
-					nums[i] = nums[i+1];
-					nums[i+1] = temp;
-				}
-			for(int i=0;i<10;i++) {
-				System.out.print(nums[i]);
+						
+		for(int j=0;j<6-1;j++) 		
+			for(int i=0;i<7-1-j;i++) 
+				if(names[i].compareTo(names[i+1]) > 0){
+					String temp;
+					temp = names[i];
+					names[i] = names[i+1];
+					names[i+1] = temp;
+					}	
+		for(int i=0;i<7;i++) {
+			System.out.printf("%s",names[i]);
+			if(i<6)
+				System.out.print(",");
 				
-				if(i<9)
-					System.out.print(",");
-				}
-				System.out.println();
-			
-			
-			
-			for(int i=0;i<8;i++)
-				if(nums[i]>nums[i+1]) {
-					int temp;
-					temp = nums[i];
-					nums[i] = nums[i+1];
-					nums[i+1] = temp;
-				}
-			for(int i=0;i<10;i++) {
-				System.out.print(nums[i]);
-				
-				if(i<9)
-					System.out.print(",");
-				}
-				System.out.println();
-				
-				
-			for(int i=0;i<7;i++)
-				if(nums[i]>nums[i+1]) {
-					int temp;
-					temp = nums[i];
-					nums[i] = nums[i+1];
-					nums[i+1] = temp;
-				}
-			for(int i=0;i<10;i++) {
-				System.out.print(nums[i]);
-				
-				if(i<9)
-					System.out.print(",");
-				}
-				System.out.println();
-				
-				
-			for(int i=0;i<6;i++)
-				if(nums[i]>nums[i+1]) {
-					int temp;
-					temp = nums[i];
-					nums[i] = nums[i+1];
-					nums[i+1] = temp;
-				}
-			for(int i=0;i<10;i++) {
-				System.out.print(nums[i]);
-				
-				if(i<9)
-					System.out.print(",");
-				}
-				System.out.println();
-				
-			
-			for(int i=0;i<5;i++)
-				if(nums[i]>nums[i+1]) {
-					int temp;
-					temp = nums[i];
-					nums[i] = nums[i+1];
-					nums[i+1] = temp;
-				}
-			for(int i=0;i<10;i++) {
-				System.out.print(nums[i]);
-				
-				if(i<9)
-					System.out.print(",");
-				}
-				System.out.println();
-			
-				
-	}
+			}
+}
 }
