1d44cdaa18211e7cdb102c4bde4452d70fd6696e
Haileykim <haileykim2014@gmail.com>
2021-03-15 12:17:14 +0900

    이차원배열 합계

---
+++ JavaPrj/src/ex3/array/Program3MultiArray3.java
@@ -0,0 +1,71 @@
+package ex3.array;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program3MultiArray3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		int count = 0;
+		String[][] rows; // 배열의 배열 [2][10] 10개짜리2개 [행][열]
+		//--------------------------------------------------------------
+		//레코드 개수를 알아 내는 코드
+		{
+			FileInputStream fis = new FileInputStream("res/corona.csv");
+			Scanner scan = new Scanner(fis);
+			
+			//while반복을 하면서 count++연산을 한다. 마지막 줄을 만날때까지
+			String line;
+			while(scan.hasNextLine()) {
+				line = scan.nextLine();
+				count ++;		
+			}			
+			
+			scan.close();
+			fis.close();
+			count--;
+			System.out.println("총 행의 갯수"+count);
+		}//충돌방지
+		//--------------------------------------------------------------
+		//레코드를 로드하는 로드
+		{
+			rows = new String[count][];
+			FileInputStream fis = new FileInputStream("res/corona.csv");
+			Scanner scan = new Scanner(fis);
+
+			String line;
+
+			scan.nextLine(); // 담을필요없다.한줄버리기
+
+			for (int i = 0; i < count; i++) { // 한줄 불러오고, 나눠서 배열에넣기 //77게
+				line = scan.nextLine(); // 한줄불러오기
+				rows[i] = line.split(","); // 불러온것 쉼표로 구분
+			}
+			scan.close();
+			fis.close();
+		}		
+		//--------------------------------------------------------------
+			//레코드 출력하는 코드
+			for (int j = 0; j < count; j++) { // 행
+				for (int i = 0; i < 10; i++) { // 열채우기
+					System.out.printf("%s", rows[j][i]);
+	
+					if (i < count-1)
+						System.out.printf(",");
+				}
+				System.out.println();
+			}
+		//--------------------------------------------------------------	
+		//검사진행자 누적수 얻는 코드
+			{
+				int total = 0;
+				
+				for(int i=0;i<count;i++)
+				total += Integer.parseInt(rows[i][3]);	
+				System.out.println(total);
+			} 
+			
+	}
+}
