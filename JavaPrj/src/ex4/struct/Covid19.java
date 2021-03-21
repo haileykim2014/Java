41a76268b35bee247200926696c378e6c92fff69
Haileykim <haileykim2014@gmail.com>
2021-03-21 21:57:07 +0900

    클래스-구조체

--- JavaPrj/src/ex4/struct/Covid19.java
+++ JavaPrj/src/ex4/struct/Covid19.java
@@ -1,13 +1,15 @@
 package ex4.struct;
 
-public class Covid19 { //사용자자료형
+public class Covid19 { //사용자자료형, 각4바이트
 	String date;
-	int positive;
+	int cases;
 	int deaths;
-	int tests;
-	
-	
-	//총12바이트
-	
+	int tests;	
+	int selfIsolation; // Camel 낙타 표기
+	int release;
+	int recover;
+	int selfQuarantineDomestic;
+	int selfQuarantineImported;
+	String pubDate;
 
 }
