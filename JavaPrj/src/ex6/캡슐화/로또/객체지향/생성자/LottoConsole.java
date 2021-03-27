086984c16c6dbcce6b900075188692ae92241ba2
Haileykim <haileykim2014@gmail.com>
2021-03-27 23:30:59 +0900

    콘솔출력 분리

---
+++ JavaPrj/src/ex6/캡슐화/로또/객체지향/생성자/LottoConsole.java
@@ -0,0 +1,14 @@
+package ex6.캡슐화.로또.객체지향.생성자;
+
+public class LottoConsole {
+	
+	public static void print(Lotto lotto) {
+		//밖에있으니까 캡슐깨지않고 함수를통해얻어야함
+		for (int i = 0; i < lotto.getSize(); i++) {
+			System.out.printf("%d", lotto.getNum(i));
+			if (i < 5)
+				System.out.printf(",");
+		}
+		System.out.println();
+	}
+}
