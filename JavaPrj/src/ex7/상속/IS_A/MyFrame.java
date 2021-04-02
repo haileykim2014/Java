c72105e169172b16630cac0f8d6b98fbdb5b1e7b
Haileykim <haileykim2014@gmail.com>
2021-04-02 23:59:40 +0900

    상속

---
+++ JavaPrj/src/ex7/상속/IS_A/MyFrame.java
@@ -0,0 +1,14 @@
+package ex7.»ó¼Ó.IS_A;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+
+public class MyFrame extends Frame {
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawLine(100, 100, 300, 300);
+				
+	}
+
+}
