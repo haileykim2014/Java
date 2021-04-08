fc18ac7edb1456381c59cb3da0a0abfdf069e6a3
Haileykim <haileykim2014@gmail.com>
2021-04-08 22:58:09 +0900

    인터페이스

---
+++ JavaPrj/src/ex9/인터페이스/exam/ybm/YBMExamConsole.java
@@ -0,0 +1,49 @@
+package ex9.인터페이스.exam.ybm;
+
+import java.util.Scanner;
+
+import ex9.인터페이스.exam.Banner;
+import ex9.인터페이스.exam.ExamConsole;
+
+public class YBMExamConsole extends ExamConsole implements Banner{
+	@Override
+	protected void onPrint() {
+		YBMExam exam = (YBMExam)getExam();
+		System.out.printf("Toeic:%d\n",exam.getToeic());
+		System.out.printf("aa::%d\n",exam.getAa());
+	}
+
+	@Override
+	public void input() {
+		super.input(); //국영수먼저 받고
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int toeic;
+		int aa;
+		
+		System.out.print("toeic:");
+		toeic = Integer.parseInt(scan.nextLine());
+		System.out.print("aa:");
+		aa = Integer.parseInt(scan.nextLine());
+		
+		YBMExam exam = (YBMExam)getExam();
+		exam.setToeic(toeic);
+		exam.setToeic(aa);
+		
+	}
+
+	@Override
+	public void printStart() {
+				
+		System.out.println("-----------------------------");
+		System.out.println("        YBM 성적 출력          ");
+		System.out.println("-----------------------------");		
+	}
+
+	@Override
+	public void printEnd() {
+		System.out.println("-----------------------------");
+		
+	}
+}
