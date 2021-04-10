deba782a2217a43fbe10667efb34d7066315afd2
Haileykim <haileykim2014@gmail.com>
2021-04-10 23:18:03 +0900

    인터페이스

---
+++ JavaPrj/src/ex9/인터페이스/exam/ybm/YBMBanner.java
@@ -0,0 +1,22 @@
+package ex9.인터페이스.exam.ybm;
+
+import ex9.인터페이스.exam.Banner;
+
+public class YBMBanner implements Banner{
+
+	@Override
+	public void print() {
+
+//		System.out.println("-----------------------------");
+//		System.out.println("        YBM 성적 출력          ");
+//		System.out.println("-----------------------------");		
+	}
+
+	@Override
+	public void printEnd() {
+
+//		System.out.println("-----------------------------");
+		
+	}
+
+}
