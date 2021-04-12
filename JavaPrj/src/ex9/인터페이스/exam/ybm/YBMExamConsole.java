98067583468f09a22a4d1ffb981964fa67bdf899
Haileykim <haileykim2014@gmail.com>
2021-04-12 23:31:46 +0900

    인터페이스-익명클래스

--- JavaPrj/src/ex9/인터페이스/exam/ybm/YBMExamConsole.java
+++ JavaPrj/src/ex9/인터페이스/exam/ybm/YBMExamConsole.java
@@ -5,7 +5,44 @@ import java.util.Scanner;
 import ex9.인터페이스.exam.Banner;
 import ex9.인터페이스.exam.ExamConsole;
 
-public class YBMExamConsole extends ExamConsole implements Banner{
+public class YBMExamConsole extends ExamConsole/* implements Banner*/{
+	private int a ;
+	
+	public YBMExamConsole() {
+//		class XXX implements Banner{
+//
+//			@Override
+//			public void print() {
+//				a = 3;
+//				System.out.println("-----------------------------");
+//				System.out.println("        YBM 성적 출력          ");
+//				System.out.println("-----------------------------");		
+//			}
+//
+//			@Override
+//			public void printEnd() {
+//				System.out.println("-----------------------------");
+//				
+//			}
+//			}
+		Banner banner = new Banner() {//인터페이스가 코드를구현하는작업을하면서 new
+			//무명클래스의 클래스정의,
+			@Override
+			public void print() {
+				System.out.println("-----------------------------");
+				System.out.println("        YBM 성적 출력          ");
+				System.out.println("-----------------------------");
+				
+			}
+
+			@Override
+			public void printEnd() {
+				System.out.println("-----------------------------");				
+			}			
+		};
+		setBanner(banner);//무명클래스의 객체
+	}
+	
 	@Override
 	protected void onPrint() {
 		YBMExam exam = (YBMExam)getExam();
@@ -32,18 +69,8 @@ public class YBMExamConsole extends ExamConsole implements Banner{
 		exam.setToeic(aa);
 		
 	}
+	//나는 Banner의 기능을 구현하려 하였으나
+	//현실은 ExamConsoled의 print를 재정의 해버리고 말았다.
+	//nested class	
 
-	@Override
-	public void printStart() {
-				
-		System.out.println("-----------------------------");
-		System.out.println("        YBM 성적 출력          ");
-		System.out.println("-----------------------------");		
-	}
-
-	@Override
-	public void printEnd() {
-		System.out.println("-----------------------------");
-		
-	}
 }
